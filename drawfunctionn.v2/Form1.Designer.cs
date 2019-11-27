dfb129ba461acb12601840d3ffb3e72fd7927988
HaPPyDutCHoGGG <42973503+HaPPyDutCHoGGG@users.noreply.github.com>
2019-11-27 19:17:28 +0300

    с№
    врврв

---
+++ drawfunctionn.v2/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace drawfunctionn
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graphWind = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.squareA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.squareB = new System.Windows.Forms.TextBox();
+            this.squareC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this._lineB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this._lineK = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.graphWind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphWind
+            // 
+            this.graphWind.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.graphWind.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.graphWind.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.graphWind.Location = new System.Drawing.Point(12, 12);
+            this.graphWind.Name = "graphWind";
+            this.graphWind.Size = new System.Drawing.Size(800, 800);
+            this.graphWind.TabIndex = 0;
+            this.graphWind.TabStop = false;
+            this.graphWind.SizeChanged += new System.EventHandler(this.GraphWind_SizeChanged);
+            this.graphWind.Click += new System.EventHandler(this.GraphWind_Click);
+            this.graphWind.Paint += new System.Windows.Forms.PaintEventHandler(this.GraphWind_Paint);
+            this.graphWind.Resize += new System.EventHandler(this.GraphWind_Resize);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(832, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "draw the graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(869, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "y = kx + b";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // squareA
+            // 
+            this.squareA.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.squareA.Location = new System.Drawing.Point(923, 222);
+            this.squareA.Name = "squareA";
+            this.squareA.Size = new System.Drawing.Size(66, 20);
+            this.squareA.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(885, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "a =";
+            // 
+            // squareB
+            // 
+            this.squareB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.squareB.Location = new System.Drawing.Point(923, 248);
+            this.squareB.Name = "squareB";
+            this.squareB.Size = new System.Drawing.Size(66, 20);
+            this.squareB.TabIndex = 5;
+            this.squareB.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // squareC
+            // 
+            this.squareC.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.squareC.Location = new System.Drawing.Point(923, 274);
+            this.squareC.Name = "squareC";
+            this.squareC.Size = new System.Drawing.Size(66, 20);
+            this.squareC.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(885, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "b =";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(885, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "c =";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(860, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "y = a*x ^ 2 + b*x + c";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(885, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "b =";
+            // 
+            // _lineB
+            // 
+            this._lineB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._lineB.Location = new System.Drawing.Point(923, 124);
+            this._lineB.Name = "_lineB";
+            this._lineB.Size = new System.Drawing.Size(66, 20);
+            this._lineB.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(885, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "k =";
+            // 
+            // _lineK
+            // 
+            this._lineK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._lineK.Location = new System.Drawing.Point(923, 98);
+            this._lineK.Name = "_lineK";
+            this._lineK.Size = new System.Drawing.Size(66, 20);
+            this._lineK.TabIndex = 10;
+            this._lineK.TextChanged += new System.EventHandler(this.TextBox5_TextChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 823);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this._lineB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this._lineK);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.squareC);
+            this.Controls.Add(this.squareB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.squareA);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.graphWind);
+            this.Name = "frmMain";
+            this.Text = "graphic of function";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.graphWind)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graphWind;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox squareA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox squareB;
+        private System.Windows.Forms.TextBox squareC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox _lineB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox _lineK;
+    }
+}
+
---
+++ drawfunctionn.v2/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drawfunctionn
+{
+    public partial class frmMain : Form //the CENTRE :(graphWind.Width / 2; graphWind.Height / 2) for programm; (0;0) for user//
+                                                        
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void GraphWind_Click(object sender, EventArgs e)
+        {
+            Graphics g = graphWind.CreateGraphics();    //horizontal axis X//
+
+            // graphWind.Width
+            // graphWind.Height
+
+            Pen myPen = new Pen(Color.Black);                                                                                                    
+                float xmin = 0;
+                float ymin = graphWind.Height / 2;
+                float xmax = graphWind.Width;
+                float ymax = graphWind.Height / 2;
+            g.DrawLine(myPen, xmin, ymin, xmax, ymax);
+
+                                                        //horizontal axis Y//
+            Pen myPen1 = new Pen(Color.Black);           
+                float xmin1 = graphWind.Width / 2;
+                float ymin1 = 0;
+                float xmax1 = graphWind.Width / 2;
+                float ymax1 = graphWind.Height;
+            g.DrawLine(myPen1, xmin1, ymin1, xmax1, ymax1);
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = graphWind.CreateGraphics();    //i dont know wat its//
+            Pen myPen = new Pen(Color.Blue);
+            int nmbInterv = 100;//100//
+                float xmin = 0;//0//
+                float xmax = 2;//2//
+                float ymin = 0;//0//
+                float ymax = 4;//4//
+            float xstep = (xmax - xmin) / nmbInterv;
+            float kx = graphWind.Width / xmax;
+            float ky = graphWind.Height / ymax;
+            float x1 = xmin;
+            float y1 = x1 * x1, x2, y2;
+            for(int i = 0; i < nmbInterv; i++)
+            {
+                x2 = x1 + xstep;
+                y2 = x2 * x2;
+                g.DrawLine(myPen, kx * x1, graphWind.Height - ky * y1, kx * x2, graphWind.Height - ky * y2);
+                x1 = x2;
+                y1 = y2;
+            }
+        }
+
+        private double _xMin = -3;
+        private double _xMax = 3;
+        private double _yMin = -3;
+        private double _yMax = 3;
+
+        private void Button2_Click(object sender, EventArgs e) //the line
+        {
+            _b = double.Parse(_lineB.Text);
+            _k = double.Parse(_lineK.Text);
+
+            drawFunction(lineFunc);
+        }
+
+        private void drawFunction(Func<double, double> func)
+        {
+            var g = graphWind.CreateGraphics();
+
+            int w = 0;
+            float prevH = calcHeight(func, w);
+
+            for (w = 1; w < graphWind.Width; w++)
+            {
+                var h = calcHeight(func, w);
+
+                g.DrawLine(_bluePen, w - 1, prevH, w, h);
+                prevH = h;
+            }
+        }
+
+        private float calcHeight(Func<double, double> func, int w)
+        {
+            var x = _xMin + (_xMax - _xMin) * w / graphWind.Width;
+
+            var y = func(x);
+
+            return (float)(graphWind.Height * (1 - (y - _yMin) / (_yMax - _yMin)));
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            _a = double.Parse(squareA.Text);
+            _b = double.Parse(squareB.Text);
+            _c = double.Parse(squareC.Text);
+
+            drawFunction(squareFunc);
+        }
+
+        private void GraphWind_Resize(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private Pen _blackPen = new Pen(Color.Black);
+        private Pen _bluePen = new Pen(Color.Blue);
+
+        private void GraphWind_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void GraphWind_SizeChanged(object sender, EventArgs e)
+        {
+            var g = graphWind.CreateGraphics();    //horizontal axis X//
+
+            // graphWind.Width
+            // graphWind.Height
+
+            g.Clear(Color.White);
+
+            float xmin = 0;                             
+            float ymin = graphWind.Height / 2;
+            float xmax = graphWind.Width;
+            float ymax = graphWind.Height / 2;
+            g.DrawLine(_blackPen, xmin, ymin, xmax, ymax);
+
+                                                         //axis Y//
+            float xmin1 = graphWind.Width / 2;                          
+            float ymin1 = 0;
+            float xmax1 = graphWind.Width / 2;
+            float ymax1 = graphWind.Height;
+            g.DrawLine(_blackPen, xmin1, ymin1, xmax1, ymax1);
+        }
+        
+        private double _a = 1;
+        private double _b = 1;
+        private double _c = 1;
+        private double _k = 1;
+
+        private double lineFunc(double x)
+        {
+            return _k * x + _b;
+        }
+
+        private double squareFunc(double x)
+        {
+            return x * (_a * x + _b) + _c;
+        }
+
+        private void  FrmMain_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            double b = double.Parse(squareB.Text);//for parabola   
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+            double b1 = double.Parse(_lineK.Text);//for line
+        }
+        
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
